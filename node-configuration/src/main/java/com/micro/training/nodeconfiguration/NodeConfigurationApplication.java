ce8bd839ae3cff4d8b37cbe58d0c1d9db74bff12
BetülSakarya <bsakarya@trupt.com.tr>
2023-11-22 12:16:33 +0300

    Initial commit

---
+++ node-configuration/src/main/java/com/micro/training/nodeconfiguration/NodeConfigurationApplication.java
@@ -0,0 +1,15 @@
+package com.micro.training.nodeconfiguration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class NodeConfigurationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NodeConfigurationApplication.class, args);
+    }
+
+}
